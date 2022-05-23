7dae9bc1a6b67e663fd9bc5dfb502f45a838c533
hemantkumar101010 <heman5502@gmail.com>
2022-05-23 21:48:59 +0530

    Library Management App Build

---
+++ FileHandlingHandsOn/ConsoleApplicationLibraryManagement/LibraryManagementSystem/LibraryManagementSystem/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("---------------WELCOME TO LIBRARY MANAGEMENT SYSTEM---------------");
+            Console.WriteLine("Please log in into any of the following account:");
+            Console.WriteLine("1. Librarian");
+            Console.WriteLine("2. Users");
+            Console.WriteLine("");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+            Librarian librarian = new Librarian();
+            Users users = new Users();
+
+            switch (choice)
+            {
+                case  1:
+                    Console.WriteLine("---------------WELCOME TO LIBRARIAN PORTAL---------------");
+                    Console.WriteLine("");
+                    Console.WriteLine("Please chooes one of the following action:");
+                    Console.WriteLine("a. Add a book in the library.");
+                    Console.WriteLine("b. Show the current available books in the library.");
+                    char choice1 = Convert.ToChar(Console.ReadLine());
+                    switch (choice1)
+                    {
+                        case 'a':
+                            librarian.AddBookToTheLibrary();
+                            break; 
+                        case 'b':
+                            librarian.ShowAvailableBooksInLibrary();
+                            break;                                                 
+                    }                    
+                     break;
+                case 2:
+                    Console.WriteLine("\t\t---------------WELCOME TO USER PORTAL---------------\t\t");
+                    Console.WriteLine("");
+                    Console.WriteLine("Please chooes one of the following action:");
+                    Console.WriteLine("a. Borrow a book from the library.");
+                    Console.WriteLine("b. See currently available books");
+                    char choice2 = Convert.ToChar(Console.ReadLine());
+                    switch (choice2)
+                    {
+                        case 'a':
+                            users.BorrowBook();
+                            break;
+                        case 'b':
+                            librarian.ShowAvailableBooksInLibrary();
+                            break;
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ FileHandlingHandsOn/ConsoleApplicationLibraryManagement/LibraryManagementSystem/LibraryManagementSystem/Users.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem
+{
+    internal  class Users
+    {
+        public string userName,bookName;
+        public int userId;
+       // DateTime issueDate;
+
+        Librarian librarian = new Librarian();
+        public void BorrowBook()
+        {
+            Console.WriteLine("Enter Your name");
+            userName = Console.ReadLine();
+
+            Console.WriteLine("Enter Your borrower Id");
+            userId = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the book name you want to borrow.");
+            bookName = Console.ReadLine();
+
+            librarian.StoreBorrowerDetails(userName, userId, bookName);
+            Console.WriteLine("Thanks for borrowing.");
+            Console.WriteLine("You hava to pay 15 rs per day of a book");
+
+        }
+
+        public void InventoryDetails(string returnDate,int userId)
+        {
+            FileStream fileStreamObj = new FileStream(@"C:\Users\iamte\Desktop\.NetTrainingAssignmentRepo\.NetTrainingAllHandsOn\FileHandlingHandsOn\ConsoleApplicationLibraryManagement\LibraryManagementSystem\borrowerDetails.txt", FileMode.Open, FileAccess.Read);
+            FileStream fileStreamWObj = new FileStream(@"C:\Users\iamte\Desktop\.NetTrainingAssignmentRepo\.NetTrainingAllHandsOn\FileHandlingHandsOn\ConsoleApplicationLibraryManagement\LibraryManagementSystem\inventoryDetails.txt", FileMode.Append, FileAccess.Write);
+
+            StreamReader streamReaderObj = new StreamReader(fileStreamObj);
+            StreamWriter streamWriterObj = new StreamWriter(fileStreamWObj);
+
+            DateTime dt1 = Convert.ToDateTime(returnDate);
+            while (streamReaderObj.Peek() > 0)
+            {
+                string line = streamReaderObj.ReadLine();
+                string[] borrowerArr = line.Split(',');
+                DateTime dt2 = Convert.ToDateTime(borrowerArr[3]);
+
+
+                if (borrowerArr[1] == Convert.ToString(userId))
+                {
+                    var t = dt1 - dt2;
+                    int rentPeriod = t.Days;
+
+                    streamWriterObj.Write(borrowerArr[1] + ",");
+                    streamWriterObj.Write(borrowerArr[0] + ",");
+                    streamWriterObj.WriteLine(rentPeriod * 15);
+                }
+
+
+            }
+            streamWriterObj.Close();
+            streamReaderObj.Close();
+            fileStreamObj.Close();
+        }
+    }
+}
