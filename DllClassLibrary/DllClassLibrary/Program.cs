18f09b3b71570039737a8bdeeafdd32beaf2f200
hemantkumar101010 <heman5502@gmail.com>
2022-05-26 17:01:16 +0530

    Dll-Class-library-handsOn-completed

---
+++ DllClassLibrary/DllClassLibrary/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EBillProject;
+
+namespace DllClassLibrary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EBill eBill = new EBill();
+           // eBill.WriteCustomerDetailsFile();
+            Customers customers = new Customers();
+            customers.ShowCustomersDetails(1);
+            Console.WriteLine();
+            Console.WriteLine();
+
+            customers.ShowEBill(4);
+            Console.WriteLine();
+            Console.WriteLine();
+
+            customers.ShowEbBillReport();
+            
+        }
+    }  
+}
---
+++ DllClassLibrary/EBillProject/Customers.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EBillProject
+{
+    public class EBill
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public int NoOfUnits { get; set; }
+        public int UnitPerCost { get; set; } = 7;
+        public int Total { get; set; }
+
+        public void WriteCustomerDetailsFile()
+        {
+            FileStream fileStreamObj = new FileStream(@"C:\Users\iamte\Desktop\.NetTrainingAssignmentRepo\.NetTrainingAllHandsOn\DllClassLibrary\EBillProject/CustomersInfo.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter streamWriterObj = new StreamWriter(fileStreamObj);
+
+            List<EBill> list = new List<EBill>();
+
+            Console.WriteLine("Enter no. of EBill objects you want to store.");
+            int totalCustomers = Convert.ToInt32(Console.ReadLine());
+
+            //initializing EBill details from inputs
+            for (int i = 0; i < totalCustomers; i++)
+            {
+                EBill ebillObj = new EBill();
+
+                Console.WriteLine("Enter Customer Id");
+                ebillObj.CustomerId = Convert.ToInt32(Console.ReadLine());
+                streamWriterObj.Write(ebillObj.CustomerId + ",");
+
+                Console.WriteLine("Enter Customer name");
+                ebillObj.CustomerName = (Console.ReadLine());
+                streamWriterObj.Write(ebillObj.CustomerName + ",");
+
+                Console.WriteLine("Enter No of units");
+                ebillObj.NoOfUnits = Convert.ToInt32(Console.ReadLine());
+                streamWriterObj.Write(ebillObj.NoOfUnits + ",");
+
+                ebillObj.Total = 7 * ebillObj.NoOfUnits;
+                streamWriterObj.WriteLine(ebillObj.Total);
+
+                list.Add(ebillObj);
+            }
+            streamWriterObj.Close();
+            fileStreamObj.Close();
+            Console.WriteLine("File write operation completed");
+
+            Console.WriteLine();
+
+        }
+    }
+    public class Customers
+    {
+               
+        public void ShowCustomersDetails(int inputCustomerId)
+        {
+            FileStream fileStreams = new FileStream(@"C:\Users\iamte\Desktop\.NetTrainingAssignmentRepo\.NetTrainingAllHandsOn\DllClassLibrary\EBillProject/CustomersInfo.txt", FileMode.Open, FileAccess.Read);
+            StreamReader streamReaderObj = new StreamReader(fileStreams);
+
+            Console.WriteLine("CustomerId\tName\t\tNoOFUnits\tTotalBill");
+            /* 1,Hemant,123,861*/
+            while (streamReaderObj.Peek() > 0)
+            {
+                string line = streamReaderObj.ReadLine();
+                if (line.StartsWith(Convert.ToString(inputCustomerId)))
+                {
+                    string[] customersArr = line.Split(',');
+                    for (int i = 0; i < customersArr.Length; i++)
+                    {
+                        Console.Write(customersArr[i]+"\t\t");
+                    }
+                    Console.WriteLine();    
+                }
+            }
+
+        }
+
+        public void ShowEBill(int inputCustomerID)
+        {
+            FileStream fileStreams = new FileStream(@"C:\Users\iamte\Desktop\.NetTrainingAssignmentRepo\.NetTrainingAllHandsOn\DllClassLibrary\EBillProject/CustomersInfo.txt", FileMode.Open, FileAccess.Read);
+            StreamReader streamReaderObj = new StreamReader(fileStreams);
+
+            Console.WriteLine("Total Bill of customer id "+ inputCustomerID);
+            /* 1,Hemant,123,861*/
+            while (streamReaderObj.Peek() > 0)
+            {
+                string line = streamReaderObj.ReadLine();
+                if (line.StartsWith(Convert.ToString(inputCustomerID)))
+                {
+                    string[] customersArr = line.Split(',');
+                   
+                        Console.Write(customersArr[3] );
+                    
+                }
+                  
+            }
+        }
+
+        public void ShowEbBillReport()
+        {
+            FileStream fileStreams = new FileStream(@"C:\Users\iamte\Desktop\.NetTrainingAssignmentRepo\.NetTrainingAllHandsOn\DllClassLibrary\EBillProject/CustomersInfo.txt", FileMode.Open, FileAccess.Read);
+            StreamReader streamReaderObj = new StreamReader(fileStreams);
+
+            Console.WriteLine("customerID\tTotalBill");
+         
+            while (streamReaderObj.Peek() > 0)
+            {
+                string line = streamReaderObj.ReadLine();
+    
+                string[] customersArr = line.Split(',');
+
+                Console.WriteLine(customersArr[0] + "\t\t" + customersArr[3]);     
+
+            }
+        }
+
+    }
+}
