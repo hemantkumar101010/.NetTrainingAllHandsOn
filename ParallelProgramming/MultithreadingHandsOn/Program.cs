01961361c9aeb80612e40398f925c35376b0d123
hemantkumar101010 <heman5502@gmail.com>
2022-05-31 17:23:59 +0530

    Multythreading-HandsOn

---
+++ ParallelProgramming/MultithreadingHandsOn/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultithreadingHandsOn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskParallelExecutions taskParallelExecutions = new TaskParallelExecutions();
+            taskParallelExecutions.DisplayCars();
+
+            //creating threads
+
+            //Thread t1 = new Thread(taskParallelExecutions.ShowBuyerReport);
+            //Thread t2 = new Thread(taskParallelExecutions.ShowDealerReport);
+
+            //stating threads
+           // t1.Start();
+            //t2.Start(); 
+            
+        }
+    }
+}
---
+++ ParallelProgramming/MultithreadingHandsOn/TaskParallelExecutions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultithreadingHandsOn
+{
+    internal class TaskParallelExecutions
+    {
+        public void ShowData(string s)
+        {
+            Console.WriteLine(s);
+        }
+        public void DisplayCars()
+        {
+            //data and functions here is executed parallerly not in a sequencial manner 
+            List<string> carCollections = new List<string>()
+            {
+                 "Lemborghini","Fortunure","Ford","Audi","Xuv"
+            };
+            Parallel.ForEach(carCollections, car => ShowData(car));
+
+            //execution of 2 function parallerly 
+            Parallel.Invoke(ShowDealerReport, ShowBuyerReport);
+
+        }
+
+        
+        public void ShowBuyerReport()
+        {
+            char buyerName = 'a';
+            for (int i = 0; i < 5; i++)
+            {               
+                Console.WriteLine(buyerName++);
+
+                //pauses for 100 mili second
+                Thread.Sleep(100);
+            }
+            
+        }
+
+        public void ShowDealerReport()
+        {
+            int dealerNumber = 1;
+            for (int i = 0; i < 5; i++)
+            {              
+                Console.WriteLine(dealerNumber++);
+                Thread.Sleep(100);      
+            }
+        }
+
+
+    }
+}
