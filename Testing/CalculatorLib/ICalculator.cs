801845628ff67744e3ef1dc2e427a1068595ae5b
hemantkumar101010 <heman5502@gmail.com>
2022-06-02 15:38:19 +0530

    unit-testing-handsOn1

---
+++ Testing/CalculatorLib/ICalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLib
+{
+    public interface ICalculator
+    {
+        int Add(int num1, int num2);
+        int Substract(int num1, int num2);
+        int Multiply(int num1, int num2);
+        int Divide(int num1, int num2);
+    }
+}
